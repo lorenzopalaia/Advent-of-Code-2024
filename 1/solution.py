2618848f8d55e09c90e2c7bcc24dde3f09c7a703
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-01 22:30:22 +0100

    1

---
+++ 1/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+with open('1/data.txt') as f:
+  input_list = [list(map(int, line.strip().split())) for line in f]
+
+left_list = []
+right_list = []
+for i in range(len(input_list)):
+  left_list.append(input_list[i][0])
+  right_list.append(input_list[i][1])
+
+# Part 1
+left_list.sort()
+right_list.sort()
+
+distance = sum(abs(l - r) for l, r in zip(left_list, right_list))
+
+print(distance)
+
+# Part 2
+from collections import Counter
+
+right_counter = Counter(right_list)
+similarity_score = sum(left_number * right_counter[left_number] for left_number in left_list)
+
+print(similarity_score)
