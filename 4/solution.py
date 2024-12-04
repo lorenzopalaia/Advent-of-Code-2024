c7c66d7e3ac2c31111bbc11f5e02e3c5fc435ece
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-04 13:59:31 +0100

    Day 4 part 1 opt

--- 4/solution.py
+++ 4/solution.py
@@ -1,79 +1,41 @@
-with open("4/data.txt") as f:
-  data = f.read().splitlines()
+data = []
 
-import re
+with open('4/data.txt', 'r') as f:
+    for line in f.readlines():
+        data.append(list(line.strip()))
+n = len(data)
 
-# Part 1
-pattern = r"XMAS"
+def is_inbounds(i, j):
+    return 0 <= i < n and 0 <= j < n
 
+# * Part 1
 res = 0
 
-horizontal = [list(x) for x in data]
-
-def count_line(data):
-  count = 0
-
-  for i in range(len(data)):
-    for j in range(len(data[i]) - 3):
-      word = "".join(data[i][j:j+4])
-      if re.match(pattern, word):
-        count += 1
-
-  return count
-
-res += count_line(horizontal)
-
-# vertical
-vertical = list(map(list, zip(*data)))
-
-res += count_line(vertical)
-
-# horizontal reversed
-horizontal_reversed = [list(reversed(x)) for x in data]
-
-res += count_line(horizontal_reversed)
-
-# vertical reversed
-vertical_reversed = [list(reversed(x)) for x in vertical]
-
-res += count_line(vertical_reversed)
-
-# diagonal to left
-diagonal_to_left = []
-for i in range(len(data)):
-  for j in range(len(data[i])):
-    if i + 3 < len(data) and j + 3 < len(data[i]):
-      diagonal_to_left.append([data[i][j], data[i+1][j+1], data[i+2][j+2], data[i+3][j+3]])
-
-res += count_line(diagonal_to_left)
-
-# diagonal to right
-diagonal_to_right = []
-for i in range(len(data)):
-  for j in range(len(data[i])):
-    if i + 3 < len(data) and j - 3 >= 0:
-      diagonal_to_right.append([data[i][j], data[i+1][j-1], data[i+2][j-2], data[i+3][j-3]])
-
-res += count_line(diagonal_to_right)
-
-# diagonal to left reversed
-diagonal_to_left_reversed = [list(reversed(x)) for x in diagonal_to_left]
-
-res += count_line(diagonal_to_left_reversed)
-
-# diagonal to right reversed
-diagonal_to_right_reversed = [list(reversed(x)) for x in diagonal_to_right]
-
-res += count_line(diagonal_to_right_reversed)
+for i in range(n):
+  for j in range(n):
+    # skip if not X, since X is the first letter of XMAS
+    if data[i][j] != 'X':
+      continue
+    # check all 8 directions arond (i, j) using (di, dj), where (0, 1) is right, (1, 0) is down, etc.
+    for di in [-1, 0, 1]:
+      for dj in [-1, 0, 1]:
+        # skip if (di, dj) is (0, 0) since it is the center
+        if (di, dj) == (0, 0):
+          continue
+        # if there's enough space to form XMAS, check if it is XMAS
+        if is_inbounds(i + 3 * di, j + 3 * dj):
+          string = ''.join(data[i + k * di][j + k * dj] for k in range(4))
+          if string == 'XMAS':
+            res += 1
 
 print(res)
 
-# Part 2
-
+# * Part 2
 def is_valid_square(data, i, j):
   # skip if center is not A    
   if data[i][j] != "A":
     return False
+  # define the 4 corners of the square
   square = [data[i - 1][j - 1], data[i - 1][j + 1], data[i + 1][j - 1], data[i + 1][j + 1]]
   return (
     (square[0] == "M" and square[1] == "M" and square[2] == "S" and square[3] == "S")
@@ -83,6 +45,7 @@ def is_valid_square(data, i, j):
   )
 
 res = 0
+
 for i in range(1, len(data) - 1):
   for j in range(1, len(data[i]) - 1):
     if is_valid_square(data, i, j):
