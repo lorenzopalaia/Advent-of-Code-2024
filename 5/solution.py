62180f693a7bd0f5f1c9dfd10e808b3f54a31d2b
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-05 15:51:33 +0100

    Day 5 part 1

---
+++ 5/solution.py
@@ -0,0 +1,33 @@
+with open("5/rules.txt", "r") as file:
+  rules = file.read().splitlines()
+
+rules = [list(map(int, rule.split("|"))) for rule in rules]
+
+with open("5/orders.txt", "r") as file:
+  orders = file.read().splitlines()
+
+orders = [list(map(int, order.split(","))) for order in orders]
+
+# * Part 1
+
+# Create a dictionary with the dependencies where the key is the first element and the value is a list of the second elements
+dependencies = {}
+for rule in rules:
+  if rule[0] not in dependencies:
+    dependencies[rule[0]] = []  
+  dependencies[rule[0]].append(rule[1])  
+
+def check_order(order, dependencies):
+  for i in range(len(order)):
+    for j in range(i+1, len(order)):      
+      if order[j] in dependencies and order[i] in dependencies[order[j]]:
+        return False
+  return True
+
+res = 0
+
+for order in orders:
+  if check_order(order, dependencies):    
+    res += order[len(order)//2]
+
+print(res)
