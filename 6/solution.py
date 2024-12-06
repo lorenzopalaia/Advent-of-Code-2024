2cd878ca0450012b328671a61aa414d214896ca0
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-06 15:48:39 +0100

    Day 6 part 2

--- 6/solution.py
+++ 6/solution.py
@@ -3,50 +3,75 @@ with open("6/data.txt", "r") as file:
 
 data = [list(line) for line in data]
 
-# * Part 1
-
+# * Constants
 WALL = "#"
 GUARD = "^"
 
-def is_inbounds(x, y):
-  return x >= 0 and x < len(data) and y >= 0 and y < len(data[0])
+# * Helper Functions
+def is_inbounds(x, y, data):
+  return 0 <= x < len(data) and 0 <= y < len(data[0])
 
 def find_guard(data):
   for i in range(len(data)):
     for j in range(len(data[0])):
       if data[i][j] == GUARD:
         return (i, j)
+  return None
 
+# * Part 1
 START = find_guard(data)
-x, y = START
-
-# initial direction is up
-dx, dy = -1, 0
-
-steps = 0
-
-# using a set to keep track of unique cells visited
 visited = set()
+x, y = START
+dx, dy = -1, 0  # initial direction: up
 
-while is_inbounds(x, y):
-  # if the next cell is a wall, turn 90 degrees to the right
-  if is_inbounds(x + dx, y + dy) and data[x + dx][y + dy] == WALL:
-    if dx == -1 and dy == 0:  # up to right
-      dx, dy = 0, 1
-    elif dx == 1 and dy == 0:  # down to left
-      dx, dy = 0, -1
-    elif dx == 0 and dy == -1:  # left to up
-      dx, dy = -1, 0
-    elif dx == 0 and dy == 1:  # right to down
-      dx, dy = 1, 0
+while is_inbounds(x, y, data):
+  # Determine the next position
+  next_x, next_y = x + dx, y + dy
 
-  # move to the next cell
-  x, y = x + dx, y + dy
-  if not is_inbounds(x, y):
-    break
-  # if the cell is not walked yet, increment the steps counter
-  if (x, y) not in visited:
+  # If there's a wall, turn 90 degrees to the right
+  if is_inbounds(next_x, next_y, data) and data[next_x][next_y] == WALL:
+    dx, dy = dy, -dx
+  else:
+    # Move forward and add the position to visited cells
+    x, y = next_x, next_y
     visited.add((x, y))
-    steps += 1
 
-print(steps)
+print(len(visited) - 1)  # Subtract 1 to exclude the starting position
+
+# * Part 2
+loops = 0
+
+for obs_x, obs_y in visited - {START}:
+  # Place an obstacle
+  data[obs_x][obs_y] = WALL
+
+  # Restart simulation
+  seen = set()
+  x, y = START
+  dx, dy = -1, 0  # initial direction: up
+  is_loop = False
+
+  while is_inbounds(x, y, data):
+    next_x, next_y = x + dx, y + dy
+
+    # If there's a wall, turn 90 degrees to the right
+    if is_inbounds(next_x, next_y, data) and data[next_x][next_y] == WALL:
+      dx, dy = dy, -dx
+    else:
+      # Move forward
+      x, y = next_x, next_y
+
+      # Check if the current state has been seen before
+      if (x, y, dx, dy) in seen:
+        is_loop = True
+        break
+
+      seen.add((x, y, dx, dy))
+
+    # Remove the obstacle
+  data[obs_x][obs_y] = "."
+
+  if is_loop:
+    loops += 1
+
+print(loops)
