7f8420a473081c63554e8bd6c2db99b57891d80c
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-07 16:16:43 +0100

    Day 7 part 2

--- 7/solution.py
+++ 7/solution.py
@@ -13,9 +13,9 @@ for line in data:
 
 # * Part 1
 
-res = 0
-
 # TODO: optimize
+
+res = 0
 
 for equation in equations:
   # calculate the number of operator positions
@@ -36,6 +36,40 @@ for equation in equations:
       # apply the multiplication
       else:
         result *= equation["value"][j+1]
+    if result == equation["key"]:
+      res += equation["key"]
+      break
+
+print(res)
+
+# * Part 2
+
+# TODO: optimize
+
+res = 0
+
+for equation in equations:
+  # calculate the number of operator positions
+  operator_positions = len(equation["value"]) - 1
+  # there are 3^operator_positions possible combinations of operators
+  for i in range(3**operator_positions):
+    operators = []
+    temp = i
+    for _ in range(operator_positions):
+      # determine if the operator is + (0), * (1) or concatenate (2)
+      operators.append(temp % 3)
+      temp //= 3
+    result = equation["value"][0]
+    for j in range(operator_positions):
+      # apply the sum
+      if operators[j] == 0:
+        result += equation["value"][j+1]
+      # apply the multiplication
+      elif operators[j] == 1:
+        result *= equation["value"][j+1]
+      # apply the concatenation
+      else:
+        result = int(str(result) + str(equation["value"][j+1]))
     if result == equation["key"]:
       res += equation["key"]
       break
