baa5dc2561ac685429a6ea521d9c7340ff27d557
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-13 06:53:48 +0100

    Day 13 part 2

--- 13/solution.py
+++ 13/solution.py
@@ -8,34 +8,39 @@ with open('13/data.txt') as f:
 # ! x = (c1b2 - c2b1) / (a1b2 - a2b1)
 # ! y = (a1c2 - a2c1) / (a1b2 - a2b1)
 
-# * Part 1
 
-
-def solve_part1():
+def solve(part):
     total_tokens = 0
+    add = 10000000000000 if part == 2 else 0
+
     for line in lines:
         if line.startswith("Button"):
             parts = line.split(" ")
             button = parts[1].split(":")[0]
             if button == 'A':
-                button_a_x = int(parts[2][2:-1])
-                button_a_y = int(parts[3][2:])
+                x1 = int(parts[2][2:-1])
+                y1 = int(parts[3][2:])
             else:
-                button_b_x = int(parts[2][2:-1])
-                button_b_y = int(parts[3][2:])
+                x2 = int(parts[2][2:-1])
+                y2 = int(parts[3][2:])
 
         elif line.startswith("Prize"):
             parts = line.split(" ")
-            prize_x = int(parts[1][2:-1])
-            prize_y = int(parts[2][2:])
-            a = (prize_x * button_b_y - prize_y * button_b_x) / \
-                (button_a_x * button_b_y - button_a_y * button_b_x)
-            b = (prize_y * button_a_x - prize_x * button_a_y) / \
-                (button_a_x * button_b_y - button_a_y * button_b_x)
+            prize_x = int(parts[1][2:-1]) + add
+            prize_y = int(parts[2][2:]) + add
+
+            a = (prize_x * y2 - prize_y * x2) / (x1 * y2 - y1 * x2)
+            b = (prize_y * x1 - prize_x * y1) / (x1 * y2 - y1 * x2)
             if a == int(a) and b == int(b):
                 total_tokens += int(3 * a + b)
 
-    print(total_tokens)
+    return total_tokens
 
 
-solve_part1()
+# * Part 1
+res = solve(1)
+print(res)
+
+# * Part 2
+res = solve(2)
+print(res)
