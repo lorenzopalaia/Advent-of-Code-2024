98262f7b9887cfe1115d96720d559f3d45d5fba3
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-18 10:28:14 +0100

    Day 18 part 2

--- 18/solution.py
+++ 18/solution.py
@@ -1,36 +1,55 @@
 with open("18/data.txt", "r") as file:
-    data = [tuple(map(int, l.strip().split(","))) for l in file]
-
+    data = [tuple(map(int, line.strip().split(","))) for line in file]
 
 START = (0, 0)
-END = (70, 70)
-
+EXIT = (70, 70)
 DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
-def maze(data):
+def find_shortest_path(data):
+    visiteds = set()
+    positions_to_visit = [(START, 0)]
 
-    visited = set()
-    to_visit = [(START, 0)]
+    while positions_to_visit:
+        current, current_distance = positions_to_visit.pop(0)
 
-    while to_visit:
-        cp, cd = to_visit.pop(0)
-
-        if cp in visited:
+        if current in visiteds:
             continue
 
-        if cp == END:
-            return cd
+        if current == EXIT:
+            return current_distance
 
         for dx, dy in DIRS:
-            np = cp[0] + dx, cp[1] + dy
-            if 0 <= np[0] <= 70 and 0 <= np[1] <= 70 and np not in visited and np not in data:
-                to_visit.append((np, cd + 1))
+            next = current[0] + dx, current[1] + dy
+            if 0 <= next[0] <= 70 and 0 <= next[1] <= 70 and next not in visiteds and next not in data:
+                positions_to_visit.append((next, current_distance + 1))
 
-        visited.add(cp)
+        visiteds.add(current)
 
     return None
 
 
-res = maze(data[:1024])
+# * Part 1
+
+# ! IDEA: Find the shortest path from START to EXIT using the first 1024 obstacles in the data.
+
+res = find_shortest_path(data[:1024])
+print(res)
+
+# * Part 2
+
+# ! IDEA: Use binary search to find the minimum number of obstacles that still allows a path from START to EXIT.
+
+lower_bound = 1024
+upper_bound = len(data)
+
+while upper_bound - lower_bound > 1:
+    mid_point = (lower_bound + upper_bound) // 2
+
+    if find_shortest_path(data[:mid_point]):
+        lower_bound = mid_point
+    else:
+        upper_bound = mid_point
+
+res = data[upper_bound - 1]
 print(res)
