556410f6a8399e7fa79396c46bf9a6911963b8f7
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-12-23 14:03:59 +0100

    Day 23 part 2

--- 23/solution.py
+++ 23/solution.py
@@ -20,7 +20,20 @@ for clique in nx.find_cliques(network_graph):
             for c in combinations(clique, 3)
             if c[0][0] == "t" or c[1][0] == "t" or c[2][0] == "t"
         )
+        # Update the largest clique if the current one is larger
+        if len(clique) > largest_clique_size:
+            largest_clique, largest_clique_size = clique, len(clique)
 
 # * Part 1
+
+# ! IDEA: Count the number of unique cliques of size 3 or more that contain at least one computer starting with 't'
+
 res = len(three_computer_cliques)
+print(res)
+
+# * Part 2
+
+# ! IDEA: Find the largest clique in the network and print the computers in it, sorted alphabetically
+
+res = ','.join(sorted(largest_clique))
 print(res)
